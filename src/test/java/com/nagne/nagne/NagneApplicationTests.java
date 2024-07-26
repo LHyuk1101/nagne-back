98670e7caa75629c13c5b5ff7f575fe1cb13e1e5
김동현 <ruggy245@naver.com>
2024-07-27 00:54:47 +0900

    first commit

---
+++ src/test/java/com/nagne/nagne/NagneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagne.nagne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NagneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
