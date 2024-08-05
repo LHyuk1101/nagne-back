bf4b6ca9f8a37d38805e878c042c32828b3cafe7
[Cloud3] 오재영 <wodud.oh@gmail.com>
2024-08-05 10:10:29 +0900

    refactory: Oauth2 로그인 시 기존 회원 여부 확인 후 회원 저장 메소드를 AuthenticationService 클래스로 이관함.

--- src/main/java/com/nagne/security/oauth/CustomAuthSuccessHandler.java
+++ src/main/java/com/nagne/security/oauth/CustomAuthSuccessHandler.java
@@ -13,6 +13,7 @@ import com.nagne.global.error.ErrorCode;
 import com.nagne.global.error.exception.ApiException;
 import com.nagne.security.config.JwtConfig;
 import com.nagne.security.jwt.JwtTokenProvider;
+import com.nagne.security.service.AuthenticationService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
@@ -34,8 +35,7 @@ public class CustomAuthSuccessHandler implements AuthenticationSuccessHandler,
     LogoutSuccessHandler {
 
     private final JwtTokenProvider jwtTokenProvider;
-    private final UserRepository userRepository;
-    private final OauthRepository oauthRepository;
+    private AuthenticationService authenticationService;
     private final ObjectMapper objectMapper;
     private final WebConfig webConfig;
     private final OAuth2AuthorizedClientService authorizedClientService;
@@ -44,13 +44,13 @@ public class CustomAuthSuccessHandler implements AuthenticationSuccessHandler,
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
         Authentication authentication) throws ServletException, IOException {
         OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+
+        String provider = oauthToken.getAuthorizedClientRegistrationId();
+        Map<String, Object> attributes = oauthToken.getPrincipal().getAttributes();
         OAuth2AuthorizedClient authorizedClient = authorizedClientService.loadAuthorizedClient(
             oauthToken.getAuthorizedClientRegistrationId(), oauthToken.getName());
 
-        Map<String, Object> attributes = oauthToken.getPrincipal().getAttributes();
-        String provider = oauthToken.getAuthorizedClientRegistrationId();
-
-        User user = findOrCreateUser(provider, attributes, authorizedClient);
+        User user = authenticationService.findOrCreateUser(provider, attributes, authorizedClient);
 
         String accessToken = jwtTokenProvider.generateAccessToken(user);
         String refreshToken = jwtTokenProvider.generateRefreshToken(user);
@@ -89,69 +89,5 @@ public class CustomAuthSuccessHandler implements AuthenticationSuccessHandler,
         response.addCookie(cookie);
 
         response.sendRedirect(webConfig.getBaseUrl());
-    }
-
-    private User findOrCreateUser(String provider, Map<String, Object> attributes,
-        OAuth2AuthorizedClient authorizedClient) {
-        final String providerId;
-        final String email;
-        final String name;
-        final String nickname;
-        final String profileImg;
-
-        switch (provider) {
-            case "google":
-                providerId = (String) attributes.get("sub");
-                email = (String) attributes.get("email");
-                name = (String) attributes.get("name");
-                profileImg = (String) attributes.get("picture");
-                nickname = name; // Google은 nickname 속성을 따로 제공하지 않음
-                break;
-
-            default:
-                throw new IllegalArgumentException("Unknown provider: " + provider);
-        }
-
-
-        // 기존 user가 존재 할 경우
-        User findUser = userRepository.findByEmail(email).orElse(null);
-        if (findUser != null) {
-            return findUser;
-        }
-
-        User user = User.builder()
-            .email(email)
-            .nickname(nickname)
-            .profileImg(profileImg)
-            .role(UserRole.USER)
-            .build();
-
-        User savedUser = userRepository.save(user);
-
-        if(savedUser == null && savedUser.getId() == null) {
-            throw new ApiException("Error occurred while creating Oauth2 user!", ErrorCode.OAUTH2_CREATE_USER_ERROR);
-        }
-
-
-        String accessToken = null;
-        String refreshToken = null;
-
-        try{
-            accessToken = authorizedClient.getAccessToken().getTokenValue();
-            refreshToken = authorizedClient.getRefreshToken().getTokenValue();
-        }catch (Exception e)
-        {
-        }
-
-        Oauthid oauthid = Oauthid.builder()
-            .provider(OauthProvider.GOOGLE)
-            .accessToken(accessToken)
-            .refreshToken(refreshToken)
-            .user(savedUser)
-            .build();
-
-        oauthRepository.save(oauthid);
-
-        return savedUser;
     }
 }
--- src/main/java/com/nagne/security/service/AuthenticationService.java
+++ src/main/java/com/nagne/security/service/AuthenticationService.java
@@ -1,13 +1,19 @@
 package com.nagne.security.service;
 
 import com.nagne.domain.user.dto.UserLoginDto;
+import com.nagne.domain.user.entity.OauthProvider;
+import com.nagne.domain.user.entity.Oauthid;
 import com.nagne.domain.user.entity.User;
+import com.nagne.domain.user.entity.UserRole;
+import com.nagne.domain.user.repository.OauthRepository;
 import com.nagne.domain.user.repository.UserRepository;
 import com.nagne.global.error.ErrorCode;
 import com.nagne.global.error.exception.ApiException;
 import com.nagne.global.error.exception.EntityNotFoundException;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -15,6 +21,7 @@ import org.springframework.stereotype.Service;
 public class AuthenticationService {
 
     private final UserRepository userRepository;
+    private final OauthRepository oauthRepository;
     private final PasswordEncoder passwordEncoder;
 
     public User login(UserLoginDto userLoginDto) throws ApiException {
@@ -26,5 +33,69 @@ public class AuthenticationService {
         }
 
         return user;
+    }
+
+    public User findOrCreateUser(String provider, Map<String, Object> attributes,
+        OAuth2AuthorizedClient authorizedClient) {
+        final String providerId;
+        final String email;
+        final String name;
+        final String nickname;
+        final String profileImg;
+
+        switch (provider) {
+            case "google":
+                providerId = (String) attributes.get("sub");
+                email = (String) attributes.get("email");
+                name = (String) attributes.get("name");
+                profileImg = (String) attributes.get("picture");
+                nickname = name; // Google은 nickname 속성을 따로 제공하지 않음
+                break;
+
+            default:
+                throw new IllegalArgumentException("Unknown provider: " + provider);
+        }
+
+
+        // 기존 user가 존재 할 경우
+        User findUser = userRepository.findByEmail(email).orElse(null);
+        if (findUser != null) {
+            return findUser;
+        }
+
+        User user = User.builder()
+            .email(email)
+            .nickname(nickname)
+            .profileImg(profileImg)
+            .role(UserRole.USER)
+            .build();
+
+        User savedUser = userRepository.save(user);
+
+        if(savedUser == null && savedUser.getId() == null) {
+            throw new ApiException("Error occurred while creating Oauth2 user!", ErrorCode.OAUTH2_CREATE_USER_ERROR);
+        }
+
+
+        String accessToken = null;
+        String refreshToken = null;
+
+        try{
+            accessToken = authorizedClient.getAccessToken().getTokenValue();
+            refreshToken = authorizedClient.getRefreshToken().getTokenValue();
+        }catch (Exception e)
+        {
+        }
+
+        Oauthid oauthid = Oauthid.builder()
+            .provider(OauthProvider.GOOGLE)
+            .accessToken(accessToken)
+            .refreshToken(refreshToken)
+            .user(savedUser)
+            .build();
+
+        oauthRepository.save(oauthid);
+
+        return savedUser;
     }
 }
