ddf382e74498d977558441097177598f7eba4ae9
김동현 <ruggy245@naver.com>
2024-08-12 01:44:30 +0900

    fix: duration null 값 해결 dto 개선

--- src/main/java/com/nagne/domain/plan/dto/PlanRequestDto.java
+++ src/main/java/com/nagne/domain/plan/dto/PlanRequestDto.java
@@ -1,20 +1,39 @@
 package com.nagne.domain.plan.dto;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
+
 public class PlanRequestDto {
   
-  private String duration;
-  private List<PlaceInfo> places;
-  private List<PlaceDistance> placeDistances;
-  private LocalDate startDay;
-  private LocalDate endDay;
-  private Integer areaCode;
+  private final int duration;
+  private final List<PlaceInfo> places;
+  private final List<PlaceDistance> placeDistances;
+  private final LocalDate startDay;
+  private final LocalDate endDay;
+  private final Integer areaCode;
+  
+  @Builder
+  public PlanRequestDto(List<PlaceInfo> places, List<PlaceDistance> placeDistances,
+    LocalDate startDay, LocalDate endDay, Integer areaCode) {
+    this.places = places;
+    this.placeDistances = placeDistances;
+    this.startDay = startDay;
+    this.endDay = endDay;
+    this.areaCode = areaCode;
+    this.duration = calculateDuration(startDay, endDay);
+  }
+  
+  private int calculateDuration(LocalDate startDay, LocalDate endDay) {
+    if (startDay != null && endDay != null) {
+      return (int) ChronoUnit.DAYS.between(startDay, endDay) + 1;
+    }
+    return 0; // 또는 적절한 기본값
+  }
   
   @Getter
   @Builder
@@ -22,9 +41,11 @@ public class PlanRequestDto {
     
     private Long id;
     private String name;
-    private String contentType;
+    private Long contentType;
     private String overview;
+    
   }
+  
   
   @Getter
   @Builder
