c6d3d137f2b66a59ca7c9fd847e6b84677760645
LCM <dlchdaud123@naver.com>
2024-08-11 16:40:10 +0900

    merge dev into feature/plan

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,80 +3,80 @@
  비밀번호 해시 전 문자열은 qwer1234!@ 입니다.
 */
 
-SET @password = '$2b$12$VUGN6yHZ8aaI2sgL4gKHguWAis/xH9oGF5JwT9.HG6Olt9X0I5NJK';
+SET
+@password = '$2b$12$VUGN6yHZ8aaI2sgL4gKHguWAis/xH9oGF5JwT9.HG6Olt9X0I5NJK';
 
 INSERT INTO users(password, email, nickname, nation, profile_img, role, term_agreed, created_at, updated_at)
-VALUES(@password, 'admin@test.com', '관리자', 410, null, 'ADMIN', true, NOW(), NOW());
+VALUES (@password, 'admin@test.com', '관리자', 410, null, 'ADMIN', true, NOW(), NOW());
 
 /*
   국가별 테스트 계정
 */
 INSERT INTO users(password, email, nickname, nation, profile_img, role, term_agreed, created_at, updated_at)
-VALUES(@password, 'test@test.com', '테스트', 410, null, 'USER', true, NOW(), NOW()),
-      (@password, 'test1@test.com', '테스트1', 840, null, 'USER', true, NOW(), NOW()), -- 미국
-      (@password, 'test2@test.com', '테스트2', 124, null, 'USER', true, NOW(), NOW()), -- 캐나다
-      (@password, 'test3@test.com', '테스트3', 250, null, 'USER', true, NOW(), NOW()), -- 프랑스
-      (@password, 'test4@test.com', '테스트4', 276, null, 'USER', true, NOW(), NOW()), -- 독일
-      (@password, 'test5@test.com', '테스트5', 392, null, 'USER', true, NOW(), NOW()), -- 일본
-      (@password, 'test6@test.com', '테스트6', 410, null, 'USER', true, NOW(), NOW()), -- 한국
-      (@password, 'test7@test.com', '테스트7', 826, null, 'USER', true, NOW(), NOW()), -- 영국
-      (@password, 'test8@test.com', '테스트8', 356, null, 'USER', true, NOW(), NOW()), -- 인도
-      (@password, 'test9@test.com', '테스트9', 554, null, 'USER', true, NOW(), NOW()), -- 뉴질랜드
-      (@password, 'test10@test.com', '테스트10', 484, null, 'USER', true, NOW(), NOW()); -- 멕시코;
+VALUES (@password, 'test@test.com', '테스트', 410, null, 'USER', true, NOW(), NOW()),
+       (@password, 'test1@test.com', '테스트1', 840, null, 'USER', true, NOW(), NOW()), -- 미국
+       (@password, 'test2@test.com', '테스트2', 124, null, 'USER', true, NOW(), NOW()), -- 캐나다
+       (@password, 'test3@test.com', '테스트3', 250, null, 'USER', true, NOW(), NOW()), -- 프랑스
+       (@password, 'test4@test.com', '테스트4', 276, null, 'USER', true, NOW(), NOW()), -- 독일
+       (@password, 'test5@test.com', '테스트5', 392, null, 'USER', true, NOW(), NOW()), -- 일본
+       (@password, 'test6@test.com', '테스트6', 410, null, 'USER', true, NOW(), NOW()), -- 한국
+       (@password, 'test7@test.com', '테스트7', 826, null, 'USER', true, NOW(), NOW()), -- 영국
+       (@password, 'test8@test.com', '테스트8', 356, null, 'USER', true, NOW(), NOW()), -- 인도
+       (@password, 'test9@test.com', '테스트9', 554, null, 'USER', true, NOW(), NOW()), -- 뉴질랜드
+       (@password, 'test10@test.com', '테스트10', 484, null, 'USER', true, NOW(), NOW());
+-- 멕시코;
 
 
-
-
- --Area 데이터 삽입
+--Area 데이터 삽입
 INSERT INTO area
     (area_code, name)
-VALUES
-    (1, 'SEOUL'),
-    (2, 'INCHEON'),
-    (3, 'DAEJEON'),
-    (4, 'DAEGU'),
-    (5, 'GWANGJU'),
-    (6, 'BUSAN'),
-    (7, 'ULSAN'),
-    (8, 'SEJONG'),
-    (31, 'GYEONGGIDO'),
-    (32, 'GANGWONDO'),
-    (33, 'CHUNGCHEONGBUKDO'),
-    (34, 'CHUNGCHEONGNAMDO'),
-    (35, 'GYEONGSANGBUKDO'),
-    (36, 'GYEONGSANGNAMDO'),
-    (37, 'JEOLLABUKDO'),
-    (38, 'JEOLLANAMDO'),
-    (39, 'JEJUDO');
+VALUES (1, 'SEOUL'),
+       (2, 'INCHEON'),
+       (3, 'DAEJEON'),
+       (4, 'DAEGU'),
+       (5, 'GWANGJU'),
+       (6, 'BUSAN'),
+       (7, 'ULSAN'),
+       (8, 'SEJONG'),
+       (31, 'GYEONGGIDO'),
+       (32, 'GANGWONDO'),
+       (33, 'CHUNGCHEONGBUKDO'),
+       (34, 'CHUNGCHEONGNAMDO'),
+       (35, 'GYEONGSANGBUKDO'),
+       (36, 'GYEONGSANGNAMDO'),
+       (37, 'JEOLLABUKDO'),
+       (38, 'JEOLLANAMDO'),
+       (39, 'JEJUDO');
 
 -- Place 데이터 삽입
-INSERT INTO place (place_id, area_code, title, address, content_id, content_type_id, overview, lat, lng, likes, modified_time, api_type) VALUES
-(1, 1, '경복궁', '서울특별시 종로구 사직로 161', 12345, 76, '조선 왕조의 법궁', 37.579617, 126.977041, 1000, '2023-08-09 10:00:00', 'TOUR'),
-(2, 1, '남산서울타워', '서울특별시 용산구 남산공원길 105', 23456, 76, '서울의 상징적인 타워', 37.551168, 126.988228, 800, '2023-08-09 10:00:00', 'TOUR'),
-(3, 1, '명동', '서울특별시 중구 명동길', 34567, 76, '쇼핑과 음식의 중심지', 37.563826, 126.983948, 1200, '2023-08-09 10:00:00', 'TOUR'),
-(4, 1, '북촌한옥마을', '서울특별시 종로구 계동길 37', 45678, 76, '전통 한옥 마을', 37.582978, 126.983685, 900, '2023-08-09 10:00:00', 'TOUR'),
-(5, 39, '성산일출봉', '제주특별자치도 서귀포시 성산읍 성산리', 56789, 76, '유네스코 세계자연유산', 33.458031, 126.942520, 1500, '2023-08-09 10:00:00', 'TOUR'),
-(6, 39, '중문관광단지', '제주특별자치도 서귀포시 중문관광로 72번길', 67890, 76, '아름다운 해변과 리조트', 33.246877, 126.412535, 1100, '2023-08-09 10:00:00', 'TOUR'),
-(7, 39, '한라산국립공원', '제주특별자치도 제주시 1100로 2070-61', 78901, 76, '제주의 상징, 한라산', 33.361900, 126.529160, 1300, '2023-08-09 10:00:00', 'TOUR'),
-(8, 39, '우도', '제주특별자치도 제주시 우도면', 89012, 76, '아름다운 섬', 33.506111, 126.951389, 1000, '2023-08-09 10:00:00', 'TOUR'),
-(9, 6, '해운대 해수욕장', '부산광역시 해운대구 해운대해변로 264', 90123, 76, '부산의 대표 해변', 35.158796, 129.160311, 1400, '2023-08-09 10:00:00', 'TOUR'),
-(10, 6, '감천문화마을', '부산광역시 사하구 감내2로 203', 12345, 76, '부산의 마추픽추', 35.097859, 129.010021, 1000, '2023-08-09 10:00:00', 'TOUR'),
-(11, 6, '자갈치시장', '부산광역시 중구 자갈치해안로 52', 23456, 82, '부산의 대표 수산시장', 35.096701, 129.030782, 800, '2023-08-09 10:00:00', 'TOUR'),
-(12, 6, '태종대', '부산광역시 영도구 전망로 24', 34567, 76, '멋진 해안 절경', 35.053435, 129.087234, 900, '2023-08-09 10:00:00', 'TOUR'),
-(13, 32, '속초해변', '강원도 속초시 조양동', 45678, 76, '동해의 아름다운 해변', 38.191368, 128.601754, 1100, '2023-08-09 10:00:00', 'TOUR'),
-(14, 32, '설악산국립공원', '강원도 속초시 설악산로 833', 56789, 76, '한국의 대표 산악 국립공원', 38.119289, 128.465549, 1300, '2023-08-09 10:00:00', 'TOUR'),
-(15, 32, '남이섬', '강원도 춘천시 남산면 남이섬길 1', 67890, 76, '아름다운 호수 섬', 37.791350, 127.525319, 1000, '2023-08-09 10:00:00', 'TOUR'),
-(16, 32, '강릉 경포대', '강원도 강릉시 저동 94', 78901, 76, '동해의 달맞이 명소', 37.805281, 128.896311, 900, '2023-08-09 10:00:00', 'TOUR');
+INSERT INTO place (place_id, area_code, title, address, content_id, content_type_id, overview, lat, lng, likes, modified_time, api_type)
+VALUES (1, 1, 'Gyeongbokgung Palace', '161 Sajik-ro, Jongno-gu, Seoul', 12345, 76, 'The main palace of the Joseon Dynasty', 37.579617, 126.977041, 1000, '2023-08-09 10:00:00', 'TOUR'),
+       (2, 1, 'Namsan Seoul Tower', '105 Namsan Park-gil, Yongsan-gu, Seoul', 23456, 76, 'Iconic tower of Seoul', 37.551168, 126.988228, 800, '2023-08-09 10:00:00', 'TOUR'),
+       (3, 1, 'Myeongdong', 'Myeongdong-gil, Jung-gu, Seoul', 34567, 76, 'Center of shopping and food', 37.563826, 126.983948, 1200, '2023-08-09 10:00:00', 'TOUR'),
+       (4, 1, 'Bukchon Hanok Village', '37 Gye-dong-gil, Jongno-gu, Seoul', 45678, 76, 'Traditional hanok village', 37.582978, 126.983685, 900, '2023-08-09 10:00:00', 'TOUR'),
+       (5, 39, 'Seongsan Ilchulbong', 'Seongsan-ri, Seogwipo-si, Jeju Special Self-Governing Province', 56789, 76, 'UNESCO World Natural Heritage', 33.458031, 126.942520, 1500, '2023-08-09 10:00:00', 'TOUR'),
+       (6, 39, 'Jungmun Tourist Complex', '72beon-gil, Jungmun Tourism Road, Seogwipo-si, Jeju Special Self-Governing Province', 67890, 76, 'Beautiful beach and resort', 33.246877, 126.412535, 1100, '2023-08-09 10:00:00', 'TOUR'),
+       (7, 39, 'Hallasan National Park', '2070-61 1100-ro, Jeju-si, Jeju Special Self-Governing Province', 78901, 76, 'Symbol of Jeju, Hallasan', 33.361900, 126.529160, 1300, '2023-08-09 10:00:00', 'TOUR'),
+       (8, 39, 'Udo', 'Udo-myeon, Jeju-si, Jeju Special Self-Governing Province', 89012, 76, 'Beautiful island', 33.506111, 126.951389, 1000, '2023-08-09 10:00:00', 'TOUR'),
+       (9, 6, 'Haeundae Beach', '264 Haeundaehaebyeon-ro, Haeundae-gu, Busan', 90123, 76, 'Representative beach of Busan', 35.158796, 129.160311, 1400, '2023-08-09 10:00:00', 'TOUR'),
+       (10, 6, 'Gamcheon Culture Village', '203 Gamnae 2-ro, Saha-gu, Busan', 12345, 76, 'Machu Picchu of Busan', 35.097859, 129.010021, 1000, '2023-08-09 10:00:00', 'TOUR'),
+       (11, 6, 'Jagalchi Market', '52 Jagalchihaean-ro, Jung-gu, Busan', 23456, 82, 'Representative fish market of Busan', 35.096701, 129.030782, 800, '2023-08-09 10:00:00', 'TOUR'),
+       (12, 6, 'Taejongdae', '24 Jeonmang-ro, Yeongdo-gu, Busan', 34567, 76, 'Magnificent coastal scenery', 35.053435, 129.087234, 900, '2023-08-09 10:00:00', 'TOUR'),
+       (13, 32, 'Sokcho Beach', 'Joyang-dong, Sokcho-si, Gangwon-do', 45678, 76, 'Beautiful beach of the East Sea', 38.191368, 128.601754, 1100, '2023-08-09 10:00:00', 'TOUR'),
+       (14, 32, 'Seoraksan National Park', '833 Seoraksan-ro, Sokcho-si, Gangwon-do', 56789, 76, 'Representative mountain national park of Korea', 38.119289, 128.465549, 1300, '2023-08-09 10:00:00', 'TOUR'),
+       (15, 32, 'Nami Island', '1 Namisum-gil, Namsan-myeon, Chuncheon-si, Gangwon-do', 67890, 76, 'Beautiful lake island', 37.791350, 127.525319, 1000, '2023-08-09 10:00:00', 'TOUR'),
+       (16, 32, 'Gyeongpodae', '94 Jeodong, Gangneung-si, Gangwon-do', 78901, 76, 'Famous moonrise spot of the East Sea', 37.805281, 128.896311, 900, '2023-08-09 10:00:00', 'TOUR');
 
 -- Plan 데이터 삽입
 INSERT INTO plans (id, user_id, area_code, status, start_day, end_day, subject, over_view, created_at, updated_at) VALUES
-(1, 1, 1, 'BEGIN', '2023-09-01', '2023-09-03', '서울 역사 탐방', '서울의 역사적 명소를 둘러보는 3일 여행', '2023-08-09 10:00:00', '2023-08-09 10:00:00'),
-(2, 2, 39, 'BEGIN', '2023-09-15', '2023-09-18', '제주도 자연 체험', '제주도의 아름다운 자연을 만끽하는 4일 여행', '2023-08-09 10:00:00', '2023-08-09 10:00:00'),
-(3, 3, 6, 'BEGIN', '2023-10-01', '2023-10-03', '부산 먹방 여행', '부산의 맛있는 음식과 명소를 즐기는 3일 여행', '2023-08-09 10:00:00', '2023-08-09 10:00:00'),
-(4, 4, 32, 'BEGIN', '2023-10-15', '2023-10-17', '강원도 힐링 여행', '강원도의 아름다운 자연에서 휴식을 취하는 3일 여행', '2023-08-09 10:00:00', '2023-08-09 10:00:00');
+                                                                                                                       (1, 1, 1, 'BEGIN', '2023-09-01', '2023-09-03', 'Seoul Historical Tour', 'A 3-day trip exploring the historical landmarks of Seoul', '2023-08-09 10:00:00', '2023-08-09 10:00:00'),
+                                                                                                                       (2, 2, 39, 'BEGIN', '2023-09-15', '2023-09-18', 'Jeju Island Nature Experience', 'A 4-day trip enjoying the beautiful nature of Jeju Island', '2023-08-09 10:00:00', '2023-08-09 10:00:00'),
+                                                                                                                       (3, 3, 6, 'BEGIN', '2023-10-01', '2023-10-03', 'Busan Foodie Trip', 'A 3-day trip enjoying the delicious food and landmarks of Busan', '2023-08-09 10:00:00', '2023-08-09 10:00:00'),
+                                                                                                                       (4, 4, 32, 'BEGIN', '2023-10-15', '2023-10-17', 'Gangwon-do Healing Trip', 'A 3-day trip relaxing in the beautiful nature of Gangwon-do', '2023-08-09 10:00:00', '2023-08-09 10:00:00');
 
 -- Template 데이터 삽입
-INSERT INTO templates (id, plan_id, place_id, days, orders, move_time, place_summary, reasoning) VALUES
+INSERT INTO templates (id, plan_id, place_id, days, orders, move_time, place_summary, reasoning)
+VALUES
 -- 서울 역사 탐방
 (1, 1, 1, 1, 1, 0, '조선 왕조의 법궁인 경복궁 관람', '서울 역사 탐방의 시작으로 조선의 중심이었던 경복궁을 방문합니다.'),
 (2, 1, 4, 1, 2, 30, '전통 한옥 마을인 북촌한옥마을 산책', '경복궁과 가까운 북촌한옥마을에서 전통 가옥을 구경하며 한국의 전통을 체험합니다.'),
