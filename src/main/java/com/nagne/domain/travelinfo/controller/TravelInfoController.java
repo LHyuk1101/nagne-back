262196e87c6ad9bd7576133b5f12454e57a1a4e4
[Cloud3] 이혁 <hyuk3852@gmail.com>
2024-08-12 17:20:30 +0900

    컨트롤러에서 서비스로직 분리

--- src/main/java/com/nagne/domain/travelinfo/controller/TravelInfoController.java
+++ src/main/java/com/nagne/domain/travelinfo/controller/TravelInfoController.java
@@ -2,19 +2,14 @@ package com.nagne.domain.travelinfo.controller;
 
 import com.nagne.domain.place.dto.PlaceDTO;
 import com.nagne.domain.place.dto.ReqPlaceDto;
-import com.nagne.domain.place.entity.Place;
 import com.nagne.domain.place.repository.PlaceRepository;
 import com.nagne.domain.travelinfo.dto.PlaceDTOforTravelInfo;
 import com.nagne.domain.travelinfo.service.TravelInfoService;
 import com.nagne.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,22 +21,16 @@ public class TravelInfoController {
 
   @GetMapping("/find/{region}")
   public ApiResponse<List<PlaceDTOforTravelInfo>> findPlacesByRegion(@PathVariable("region") String region) {
-
-    // 각 contentTypeId에 대해 10건씩 가져오기 위한 Pageable 설정
-    Pageable pageable = PageRequest.of(0, 10);
-
-    // 두 contentTypeId에 대한 상위 10개의 결과를 각각 가져옴
-    List<PlaceDTOforTravelInfo> top10ContentTypeId76 = placeRepository.findTop10ByRegionAndContentTypeId76OrderByLikes(
-            region, pageable);
-    List<PlaceDTOforTravelInfo> top10ContentTypeId82 = placeRepository.findTop10ByRegionAndContentTypeId82OrderByLikes(
-            region, pageable);
-
-    // 결과를 합침
-    List<PlaceDTOforTravelInfo> combinedResults = new ArrayList<>();
-    combinedResults.addAll(top10ContentTypeId76);
-    combinedResults.addAll(top10ContentTypeId82);
+    List<PlaceDTOforTravelInfo> combinedResults = travelInfoService.findPlacesByRegion(region);
     return ApiResponse.success(combinedResults);
+  }
 
+  @GetMapping("/find/{contentTypeId}/{areaCode}")
+  public ApiResponse<List<PlaceDTOforTravelInfo>> findPlaces(@PathVariable Long contentTypeId,
+                                                             @PathVariable Integer areaCode) {
+    List<PlaceDTOforTravelInfo> result = travelInfoService.findPlaces(contentTypeId, areaCode);
+
+    return ApiResponse.success(result);
   }
 
 
@@ -50,24 +39,6 @@ public class TravelInfoController {
     List<PlaceDTO> places = travelInfoService.fetchPlaceByAreaName(reqPlaceDto);
     return ApiResponse.success(places);
   }
-
-  @GetMapping("/find/{contentTypeId}/{areaCode}")
-  public List<PlaceDTO> findPlaces(@PathVariable Long contentTypeId,
-                                   @PathVariable Integer areaCode) {
-
-    List<Place> places = placeRepository.findByContentTypeIdAndArea_AreaCode(contentTypeId,
-            areaCode);
-
-    return places.stream().map(place ->
-            PlaceDTO.builder()
-                    .id(place.getId())
-                    .title(place.getTitle())
-                    .areaCode(place.getArea().getAreaCode())
-                    .overview(place.getOverview())
-                    .build()).collect(Collectors.toList());
-
-  }
-
 
   @GetMapping("/findall/{region}")
   public List<PlaceDTOforTravelInfo> findAllPlacesByRegion(@PathVariable("region") String region) {
