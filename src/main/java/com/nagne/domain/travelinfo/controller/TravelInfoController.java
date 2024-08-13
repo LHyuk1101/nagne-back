eaa673826bbe69194d6e2a168783ca3c02309508
[Cloud3] 이혁 <hyuk3852@gmail.com>
2024-08-14 01:38:29 +0900

    Refator findByRegion 메서드 사용하는 방식으로 변경, PlaceRepository 공통된 쿼리 메서드 삭제

--- src/main/java/com/nagne/domain/travelinfo/controller/TravelInfoController.java
+++ src/main/java/com/nagne/domain/travelinfo/controller/TravelInfoController.java
@@ -1,18 +1,16 @@
 package com.nagne.domain.travelinfo.controller;
 
+import com.nagne.domain.place.dto.PlaceDTO;
 import com.nagne.domain.place.dto.ReqPlaceDto;
 import com.nagne.domain.place.dto.ResponsePlaceDto;
 import com.nagne.domain.place.repository.PlaceRepository;
 import com.nagne.domain.travelinfo.dto.PlaceDTOforTravelInfo;
 import com.nagne.domain.travelinfo.service.TravelInfoService;
 import com.nagne.global.response.ApiResponse;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,16 +20,16 @@ public class TravelInfoController {
   private final PlaceRepository placeRepository;
   private final TravelInfoService travelInfoService;
 
-  @GetMapping("/find/{region}")
-  public ApiResponse<List<PlaceDTOforTravelInfo>> findPlacesByRegion(
-    @PathVariable("region") String region) {
-    List<PlaceDTOforTravelInfo> combinedResults = travelInfoService.findPlacesByRegion(region);
-    return ApiResponse.success(combinedResults);
+  @GetMapping("/find/{areaCode}")
+  public ApiResponse<List<PlaceDTO>> findPlacesByRegion(
+          @PathVariable("areaCode") int areaCode) {
+    List<PlaceDTO> placesByRegion = travelInfoService.findPlacesByRegion(areaCode);
+    return ApiResponse.success(placesByRegion);
   }
 
   @GetMapping("/find/{contentTypeId}/{areaCode}")
   public ApiResponse<List<PlaceDTOforTravelInfo>> findPlaces(@PathVariable Long contentTypeId,
-    @PathVariable Integer areaCode) {
+                                                             @PathVariable Integer areaCode) {
     List<PlaceDTOforTravelInfo> result = travelInfoService.findPlaces(contentTypeId, areaCode);
 
     return ApiResponse.success(result);
