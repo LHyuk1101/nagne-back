0e973c84f9b019dac5f48366f3aa07f0929214c9
김동현 <ruggy245@naver.com>
2024-08-14 19:30:50 +0900

    refector: plans_id -> plan_id 네임변경

--- src/main/java/com/nagne/domain/template/entity/Template.java
+++ src/main/java/com/nagne/domain/template/entity/Template.java
@@ -44,7 +44,7 @@ public class Template {
   private String reasoning;
   
   @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "plans_id")
+  @JoinColumn(name = "plan_id")
   private Plan plan;
   
   @ManyToOne(fetch = FetchType.LAZY)
