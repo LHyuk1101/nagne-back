265db3b717add18b08b0177d4485752a29fc8999
ruu <wjrdjs005@naver.com>
2024-08-15 15:35:46 +0900

    remove & refector: 필요없는 메서드 삭제 및 함수명 변경

--- src/main/java/com/nagne/domain/place/repository/PlaceRepository.java
+++ src/main/java/com/nagne/domain/place/repository/PlaceRepository.java
@@ -2,10 +2,8 @@ package com.nagne.domain.place.repository;
 
 import com.nagne.domain.place.dto.PlaceDTO;
 import com.nagne.domain.place.entity.Place;
-import com.nagne.domain.place.entity.PlaceImg;
 import com.nagne.domain.travelinfo.dto.PlaceDTOforTravelInfo;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -29,8 +27,6 @@ public interface PlaceRepository extends JpaRepository<Place, Long>, PlaceReposi
   // Area 엔티티의 areaCode 필드를 참조하도록 수정
   List<Place> findByContentTypeIdAndArea_AreaCode(Long contentTypeId, Integer areaCode);
 
-  List<Place> findByArea_AreaCode(Integer areaCode);
-
   @Query(
     "SELECT DISTINCT new com.nagne.domain.place.dto.PlaceDTO(p.id, p.area, p.title, p.address, "
       + "p.contentTypeId, p.overview, COALESCE(s.contactNumber, ''), COALESCE(s.openTime, ''), p.lat, p.lng, p.likes, p.thumbnailUrl, pi.imgUrl ) "
@@ -40,24 +36,9 @@ public interface PlaceRepository extends JpaRepository<Place, Long>, PlaceReposi
       + "WHERE p.contentTypeId IN :regionIds "
       + "AND p.area.areaCode = :areaCode "
       + "ORDER BY p.likes DESC, p.id")
-  List<PlaceDTO> findByRegion(@Param("regionIds") Long[] regionIds, @Param("areaCode") int areaCode,
-
+  List<PlaceDTO> findTopLikesByRegion(@Param("regionIds") Long[] regionIds,
+    @Param("areaCode") int areaCode,
     Pageable pageable);
-
-
-  @Query("SELECT count(*) "
-    + "FROM Place p "
-    + "WHERE p.contentTypeId IN :regionIds "
-    + "AND p.area.areaCode = :areaCode")
-  int getTotalCountByRegion(@Param("regionIds") Long[] regionIds, @Param("areaCode") int areaCode);
-
-  @Query("SELECT pi "
-    + "FROM PlaceImg pi "
-    + "WHERE pi.place.id = :id")
-  List<PlaceImg> findByPlaceId(Long id);
-
-
-  Optional<Place> findByTitle(String title);
 
   @Query(
     "SELECT new com.nagne.domain.travelinfo.dto.PlaceDTOforTravelInfo(p.id, p.area, p.title, p.address, "
